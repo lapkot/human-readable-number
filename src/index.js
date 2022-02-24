07feeb4a9a975a95c6ecdfd809d8ef32a4818f53
lapkot <byyy.com@gmail.com>
2022-02-24 21:01:27 +0100

    feat: add implementation for converter

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-module.exports = function toReadable (number) {
-  
+function toReadable (number) {
+    const arr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    const arr2 = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    if (number < 20) {
+        return arr[number];
+    }
+    if (number >= 20 && number < 100) {
+        const rest = number % 10;
+        return `${arr2[Math.floor((number / 10) - 2)]}${rest !== 0 ? ` ${arr[rest]}` : ''}`;
+    }
+    if (number >= 100) {
+        const rest100 = number % 100;
+        const rest10 = number % 10;
+        const dif = Math.floor((rest100 - rest10) / 10);
+        let res = `${arr[Math.floor(number / 100)]} hundred`
+        if (rest100 < 20 && rest100 > 0) {
+            res += ` ${arr[rest100]}`;
+            return res;
+        }
+        if (dif !== 0) {
+            res += ` ${arr2[dif - 2]}`;
+        }
+        if (rest10 !== 0) {
+            res += ` ${arr[rest10]}`;
+        }
+        return res;
+    }
 }
+
+module.exports = toReadable;
+
+console.log(toReadable(245))
